94a4f3bf986bd47fd05b76bb91a8b7e2f54c1c9f
Dornex <43918692+Dornex@users.noreply.github.com>
2019-03-18 09:43:18 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "administrator.h"
 
 typedef struct history{
+	char istoric[70];
 	struct history *nextHistory;
 }history;
 
@@ -31,7 +31,7 @@ typedef struct LSC{
 
 void add_LSC(LSC **lsc, unsigned int poz)
 {
- 	LSC *elem = (LSC*)malloc(sizeof(LSC));
+ 	LSC *elem = (LSC*)calloc(1, sizeof(LSC));
  	elem -> poz = poz;
  	elem -> nr_max_carduri = (*lsc) -> nr_max_carduri;
  	elem -> nextLSC = (*lsc);
@@ -53,7 +53,7 @@ int verif_duplicate(LSC *lsc, unsigned int poz, char card_number[30])
 		CARD* card = p->nextCard;
 		while(card!=NULL)
 		{
-			if(strcmp(card->card_number, card_number) == 0) return 1;
+            if(strcmp(card->card_number, card_number) == 0) return 1;
 			card = card->nextCard;
 		}
 	}
@@ -85,8 +85,8 @@ CARD* find_card(LSC *lsc, char card_number[30])
 	}
 
 	CARD* c = p->nextCard;
-	if(p!=NULL) 
-	{		
+	if(p!=NULL)
+	{
 		while(c!=NULL)
 		{
 			if(strcmp(c->card_number, card_number) == 0) break;
@@ -104,7 +104,7 @@ void add_card(char card_number[30], char pin[6], char expiry_date[10], int cvv, 
 	else
 	{
 		LSC *p = (*lsc);
-		CARD *card = (CARD*)malloc(sizeof(CARD));
+		CARD *card = (CARD*)calloc(1,sizeof(CARD));
 
 		while(p != NULL)
 		{
@@ -122,18 +122,18 @@ void add_card(char card_number[30], char pin[6], char expiry_date[10], int cvv, 
 		card -> inserted = 0;
 		card -> cvv = cvv;
 		card -> balance = 0;
-		
+
 		if(p == NULL)
 		{
 			add_LSC(lsc, poz);
 			(*lsc)->nextCard = card;
 		}
 		else
-		{	
+		{
 			card -> nextCard = p->nextCard;
 			p -> nextCard = card;
 		}
-	} 
+	}
 }
 
 void afisare(LSC *lsc)
@@ -156,7 +156,7 @@ void insert_card(LSC *lsc, char card_number[30], char pin[6])
 {
 	CARD *c = find_card(lsc, card_number);
 
-	if(c->strike == 3) 
+	if(c->strike == 3)
 	{
 		printf("The card is blocked. Please contact the administrator\n");
 		strcpy(c->status, "BLOCKED");
@@ -167,10 +167,11 @@ void insert_card(LSC *lsc, char card_number[30], char pin[6])
 			c->strike++;
 		}
 	else if(strcmp(c->pin, c->pin_initial) == 0)
-	{		
-		c->inserted = 1;			
+	{
+		c->inserted = 1;
 		c->strike = 0;
 		printf("You must change your PIN!\n");
+		add_history()
 	}
 	else if(strcmp(c->pin, c->pin_initial) != 0) c->inserted = 1;
 }
@@ -187,22 +188,22 @@ void recharge(LSC *lsc, char card_number[30], unsigned int suma)
 	if(c->inserted == 1)
 	{
 		if(suma % 10 != 0) printf("The added amount must be multiple of 10!\n");
-		else 
+		else
 		{
-			
+
 			c->balance += suma;
 			printf("Sold curent: %d\n", c->balance);
 		}
 		cancel(lsc, card_number);
 	}
-	else printf("The card is not inserted!\n");	
+	else printf("The card is not inserted!\n");
 }
 
 void cash_withdrawal(LSC *lsc, char card_number[30], unsigned int suma)
 {
 	CARD* c = find_card(lsc, card_number);
 	if(c->inserted == 1)
-	{	
+	{
 		if(suma > c->balance) printf("Insufficient funds!\n");
 		else if(suma % 10 != 0) printf("The requested amount must be multiple of 10!\n");
 		else
@@ -219,11 +220,39 @@ void balance_inquiry(LSC *lsc, char card_number[30])
 {
 	CARD* c = find_card(lsc, card_number);
 	if(c->inserted == 1)
-	{	
+	{
 		printf("Sold: %d\n", c->balance);
 		cancel(lsc, card_number);
 	}
 	else printf("The card is not inserted!\n");
+}
+
+void pin_change(LSC *lsc, char card_number[30], char pin[6])
+{
+	if(strlen(pin)!=4) printf("Invalid PIN!\n");
+	else
+	{
+		CARD *c = find_card(lsc, card_number);
+		strcpy(c->pin, pin);
+	}
+	cancel(lsc, card_number);
+}
+
+void transfer_funds(LSC* lsc, char card_number_source[30], char card_number_dest[30], unsigned int suma)
+{
+	CARD* card_sursa = find_card(lsc, card_number_source);
+	CARD* card_dest = find_card(lsc, card_number_dest);
+
+	if(suma % 10 != 0) {printf("The trasnfered amount must be multiple of 10!\n"); cancel(lsc, card_number_source);}
+	else if(suma > card_sursa->balance) {printf("Insufficient funds!\n"); cancel(lsc, card_number_source);}
+	else
+	{
+		card_sursa->balance -= suma;
+		card_dest->balance += suma;
+		printf("Sold ramas: %d\n", card_sursa->balance);
+		printf("Sold dest: %d\n", card_dest->balance);
+		cancel(lsc, card_number_source);
+	}
 }
 
 int main()
@@ -235,29 +264,26 @@ int main()
 	fscanf(fisier_in, "%d", &nr_max_carduri);
 	fprintf(fisier_out, "%d\n", nr_max_carduri);
 
-	LSC *lsc = (LSC*)malloc(sizeof(LSC));
+	LSC *lsc = (LSC*)calloc(1, sizeof(LSC));
 	lsc -> nr_max_carduri = nr_max_carduri;
 
 
 	while(!feof(fisier_in))
 	{
-
 		char optiune[20], expiry_date[20];
 		char card_number[30], pin[10];
-		int cvv = 0; 
+		int cvv = 0;
 
 		fscanf(fisier_in, "%s", optiune);
 		if(strcmp(optiune, "add_card") == 0)
-		{	
+		{
 			fscanf(fisier_in, "%s %s %s %d", card_number, pin, expiry_date, &cvv);
-			add_card(card_number, pin, expiry_date, cvv, "Gigel", &lsc);	
-			fflush(fisier_in);
+			add_card(card_number, pin, expiry_date, cvv, "Gigel", &lsc);
 		}
-		else if(strcmp(optiune, "insert_card") == 0) 
+		else if(strcmp(optiune, "insert_card") == 0)
 			{
 				fscanf(fisier_in, "%s %s", card_number, pin);
 				insert_card(lsc, card_number, pin);
-				fflush(fisier_in);
 			}
 			else if(strcmp(optiune, "recharge") == 0)
 			{
@@ -276,7 +302,19 @@ int main()
 				fscanf(fisier_in, "%s", card_number);
 				balance_inquiry(lsc, card_number);
 			}
-		strcpy(optiune, "");
+			else if(strcmp(optiune, "pin_change") == 0)
+			{
+				fscanf(fisier_in, "%s %s", card_number, pin);
+				pin_change(lsc, card_number, pin);
+			}
+			else if(strcmp(optiune, "transfer_funds") == 0)
+			{
+				unsigned int suma;
+				char card_number_source[30], card_number_dest[30];
+				fscanf(fisier_in, "%s %s %d", card_number_source, card_number_dest, &suma);
+				transfer_funds(lsc, card_number_source, card_number_dest, suma);
+			}
+			strcpy(optiune, "");
 	}
 
 	return 0;
